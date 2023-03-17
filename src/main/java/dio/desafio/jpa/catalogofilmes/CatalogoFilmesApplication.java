49c060a6c47f1601bf9739d4c447b2ef0a78d3aa
Luiz Roberto <luizrobertoao29@gmail.com>
2023-03-17 14:09:31 -0300

    Primeiro commit.

---
+++ src/main/java/dio/desafio/jpa/catalogofilmes/CatalogoFilmesApplication.java
@@ -0,0 +1,13 @@
+package dio.desafio.jpa.catalogofilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoFilmesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/desafio/jpa/catalogofilmes/CatalogoFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.desafio.jpa.catalogofilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
